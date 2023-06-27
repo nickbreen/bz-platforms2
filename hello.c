2dabcb80dca5cadb18333d8acb517953a54fe960
nickbreen <nick@breen.kiwi>
2023-06-27 13:19:29 +1200

    yoink flags from @local_config_cc on Ubuntu/lunar
    add __DATE__ and __TIME__ to hello.c so we can see that bazel's default values of "redacted" are not present.

--- hello.c
+++ hello.c
@@ -2,5 +2,5 @@
 #include <gnu/libc-version.h>
 
 int main() {
-   printf("Hello! I was compiled against GNU libc %u.%u with GNU C %s!\n", __GLIBC__, __GLIBC_MINOR__, __VERSION__);
+   printf("Hello! I was compiled against GNU libc %u.%u with GNU C %s at %s %s!\n", __GLIBC__, __GLIBC_MINOR__, __VERSION__, __DATE__, __TIME__);
 }
